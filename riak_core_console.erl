e6a25da773c80fc4ecaf172a856e98650d7f3bc9
Heinz N. Gies <heinz@licenser.net>
2012-12-27 19:08:12 +0100

    Made all components xref-clean.

--- riak_core_console.erl
+++ riak_core_console.erl
@@ -4,6 +4,10 @@
          leave/1,
          remove/1,
          ringready/1]).
+-ignore_xref([join/1,
+              leave/1,
+              remove/1,
+              ringready/1]).
 
 join([NodeStr]) ->
     try
--- riak_core_wm_ping.erl
+++ riak_core_wm_ping.erl
@@ -1,5 +1,6 @@
 -module({{appid}}_wm_ping).
 -export([init/1, to_html/2]).
+-ignore_xref([init/1, to_html/2, ping/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
